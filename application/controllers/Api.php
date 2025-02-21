0c9fd6de3d09e333b0d1ca4501ff1ea0d54dfecd
bhoomitech <bhoomitechzone@gmail.com>
2025-02-21 18:28:07 +0530

    Caluclate the distance

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -21,6 +21,28 @@ class Api extends CI_Controller
         return $input ? $input : $this->input->post();
     }
 
+    private function calculateDistance($lat1, $lon1, $lat2, $lon2)
+    {
+        $earthRadius = 6371;
+
+        $lat1 = deg2rad($lat1);
+        $lon1 = deg2rad($lon1);
+        $lat2 = deg2rad($lat2);
+        $lon2 = deg2rad($lon2);
+
+        $dlat = $lat2 - $lat1;
+        $dlon = $lon2 - $lon1;
+
+        $a = sin($dlat / 2) * sin($dlat / 2) +
+            cos($lat1) * cos($lat2) *
+            sin($dlon / 2) * sin($dlon / 2);
+
+        $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+
+        return $earthRadius * $c;
+    }
+
+
     public function Test()
     {
 
@@ -74,6 +96,7 @@ class Api extends CI_Controller
         $user_lon = $input['user_lon'] ?? null;
         $current_date = $input['current_date'] ?? null;
         $current_time = $input['current_time'] ?? null;
+
         if (empty($branch_id)) {
             echo json_encode(['res' => 'error', 'msg' => 'Branch Id is required.']);
             return;
@@ -120,6 +143,11 @@ class Api extends CI_Controller
             echo json_encode(['res' => 'error', 'msg' => 'Branch not exist.']);
             return;
         }
+
+        $distance = $this->calculateDistance($branch_lat, $branch_lon, $user_lat, $user_lon);
+
+        $distance_in_meter = $distance * 1000;
+        echo json_encode(['res' => 'success', 'distance_meter' => $distance_in_meter]);
 
 
 
