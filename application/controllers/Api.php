2561a2b258aeaf8c9b81002f6bdd2a4147a8601a
bhoomitech <bhoomitechzone@gmail.com>
2025-02-21 18:36:56 +0530

    Round figure the distance

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -146,7 +146,7 @@ class Api extends CI_Controller
 
         $distance = $this->calculateDistance($branch_lat, $branch_lon, $user_lat, $user_lon);
 
-        $distance_in_meter = $distance * 1000 . "meter";
+        $distance_in_meter = round($distance * 1000) . " " . "meter";
         
         if($distance_in_meter > 100){
             echo json_encode(['res' => 'error', 'data'=>$distance_in_meter, 'msg' => 'You are not in branch location.']);
