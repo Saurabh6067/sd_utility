1a0d8787129e6e8da7c3b442706f4c5687f044f4
bhoomitech <bhoomitechzone@gmail.com>
2025-02-21 18:40:11 +0530

    check else

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -147,12 +147,12 @@ class Api extends CI_Controller
         $distance = $this->calculateDistance($branch_lat, $branch_lon, $user_lat, $user_lon);
 
         $distance_in_meter = round($distance * 1000) . " " . "meter";
-        
-        if($distance_in_meter > 100){
-            echo json_encode(['res' => 'error', 'data'=>$distance_in_meter, 'msg' => 'You are not in branch location.']);
-            return;
-        }else{
 
+        if ($distance_in_meter > 100) {
+            echo json_encode(['res' => 'error', 'data' => $distance_in_meter, 'msg' => 'You are not in branch location.']);
+            return;
+        } else {
+            echo json_encode(['res' => 'success', 'data' => $distance_in_meter, 'msg' => 'You arr in branch location.']);
         }
 
 
