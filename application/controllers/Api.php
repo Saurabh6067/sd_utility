b58f0951ee2620ad7af5e08d21c8e4cb7d38bbea
bhoomitech <bhoomitechzone@gmail.com>
2025-02-25 15:04:32 +0530

    chaafhgashsdgrrshdh

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -206,6 +206,34 @@ class Api extends CI_Controller
             echo json_encode(['res' => 'error', 'msg' => 'Invalid Attendance Type.']);
         }
     }
+
+    public function checkPunchInPunchOut(){
+        $input = $this->getJsonInput();
+        $user_id = $input['user_id'] ?? null;
+        $current_date = date('Y-m-d'); 
+    
+        if (!$user_id) {
+            echo json_encode(['res' => 'error', 'msg' => 'Employee ID is required.']);
+            return;
+        }
+    
+        $this->db->where('user_id', $user_id);
+        $this->db->where('today_date', $current_date);
+        $attendance = $this->db->get('attendance')->row_array();
+    
+        if ($attendance) {
+            if (empty($attendance['punch_in_date']) || empty($attendance['punch_in_time'])) {
+                echo json_encode(['res' => 'error', 'msg' => 'User has not punched in today.']);
+            } elseif (empty($attendance['punch_out_date']) || empty($attendance['punch_out_time'])) {
+                echo json_encode(['res' => 'error', 'msg' => 'User has not punched out today.']);
+            } else {
+                echo json_encode(['res' => 'success', 'data' => $attendance, 'msg' => 'User has completed both Punch In and Punch Out.']);
+            }
+        } else {
+            echo json_encode(['res' => 'error', 'msg' => 'No attendance record found for today.']);
+        }
+    }
+    
     
 }
 ?>
